f75b793f9f99402abd4c925a3294d3496b047d04
NotSoFriendlyGhost <131089264+NotSoFriendlyGhost@users.noreply.github.com>
2024-03-04 11:40:50 -0600

    Changed comment formatting

--- include/titantron/joystickCurves.hpp
+++ include/titantron/joystickCurves.hpp
@@ -1,19 +1,19 @@
 #pragma once
 
 /*
- * @brief Transforms the joystick value for the left joystick
+ * Transforms the joystick value for the left joystick
  * 
- * @param stickInput Input from the joystick
+ * \param stickInput Input from the joystick
  * 
- * @return Returns transformed joystick value
+ * \return Returns transformed joystick value
  */
 double opcontrolLeftCurve(double stickInput); 
 
 /*
- * @brief Transforms the joystick value for the right joystick
+ * Transforms the joystick value for the right joystick
  * 
- * @param stickInput Input from the joystick
+ * \param stickInput Input from the joystick
  * 
- * @return Returns transformed joystick value
+ * \return Returns transformed joystick value
  */
 double opcontrolRightCurve(double stickInput); 
