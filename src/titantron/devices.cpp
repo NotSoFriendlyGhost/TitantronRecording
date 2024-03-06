1702ee19fd459f45ec179284830adc91a7a79302
NotSoFriendlyGhost <131089264+NotSoFriendlyGhost@users.noreply.github.com>
2024-03-05 18:03:19 -0600

    Setting motor encoders causing problems with joystick control

--- src/titantron/devices.cpp
+++ src/titantron/devices.cpp
@@ -1,9 +1,9 @@
 #include "main.h"
 
-pros::Motor leftFront(1,pros::E_MOTOR_GEAR_GREEN,true, pros::E_MOTOR_ENCODER_COUNTS);
-pros::Motor leftBack(12,pros::E_MOTOR_GEAR_GREEN, true, pros::E_MOTOR_ENCODER_COUNTS);
-pros::Motor rightFront(10, pros::E_MOTOR_GEAR_GREEN, pros::E_MOTOR_ENCODER_COUNTS);
-pros::Motor rightBack(18, pros::E_MOTOR_GEAR_GREEN, pros::E_MOTOR_ENCODER_COUNTS);
+pros::Motor leftFront(1,pros::E_MOTOR_GEAR_GREEN,true);
+pros::Motor leftBack(12,pros::E_MOTOR_GEAR_GREEN, true);
+pros::Motor rightFront(10, pros::E_MOTOR_GEAR_GREEN);
+pros::Motor rightBack(18, pros::E_MOTOR_GEAR_GREEN);
 
 pros::Motor intake(5);
 pros::Motor flywheel(15, pros::E_MOTOR_GEAR_BLUE, true);
